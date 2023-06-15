a3e08f739ef73d68ae32370400d5ad6ed37a8132
Liren Jin <ljin@uni-bonn.de>
2023-06-15 11:22:57 +0200

    refactor code into a ros workspace

--- run_bridge.sh
+++
@@ -1,4 +0,0 @@
-rosrun ros_ign_bridge parameter_bridge /rgbd/image@sensor_msgs/Image@ignition.msgs.Image \
-                                       /rgbd/depth_image@sensor_msgs/Image@ignition.msgs.Image \
-                                       /semantic/labels_map@sensor_msgs/Image@ignition.msgs.Image \
-                                       rgbd/camera_info@sensor_msgs/CameraInfo@ignition.msgs.CameraInfo
